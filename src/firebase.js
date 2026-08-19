844350ac6f33b89f7e794e05144622df9ae482a0
Kaif Ali <rkaif8314@gmail.com>
2026-08-20 02:58:18 +0530

    feat: configure Firebase Auth and Firestore

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCguj1eDGdctoWCoJl8cJStrAs4UVfmQYw',
+  authDomain: 'codevault-dc8ac.firebaseapp.com',
+  projectId: 'codevault-dc8ac',
+  storageBucket: 'codevault-dc8ac.firebasestorage.app',
+  messagingSenderId: '692689628979',
+  appId: '1:692689628979:web:968d3c5c10ae401a86d846',
+  measurementId: 'G-DMJ474ZC07',
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
